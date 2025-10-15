af625cd6eccbb9dd3880ca9b031b2531e3580059
Ameet Khemani <ameetkhemani@gmail.com>
2025-10-15 09:38:13 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@RestController
+public class UserController {
+
+	private final UserRepository userRepository;
+
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	// 1️⃣ Welcome API
+	@GetMapping
+	public String welcome() {
+		return "Welcome to Spring Boot + PostgreSQL demo!";
+	}
+
+	// 2️⃣ Register user
+	@PostMapping("/user")
+	public String addUser(@RequestBody User user) {
+		userRepository.save(user);
+		return "User added successfully!";
+	}
+
+	// 3️⃣ Get all users
+	@GetMapping("/users")
+	public List<String> getAllUsers() {
+		return userRepository.findAll().stream().map(User::getName).collect(Collectors.toList());
+	}
+}
